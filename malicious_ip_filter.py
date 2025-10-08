af2a74cc6fdb3169be04624a81056a434ce66c3a
Gaurav Chandrasekhar <chandrasekhargaurav@gmail.com>
2025-10-08 16:45:00 +0530

    updated malicious_ip_filter.py,search_indexer.py

--- malicious_ip_filter.py
+++ malicious_ip_filter.py
@@ -7,7 +7,7 @@ class MaliciousIPFilter:
     blacklist of IP addresses. It calculates its own optimal parameters (m and k) 
     and uses double hashing.
     """
-
+    # m,n- sizes of the bit arrays.
     def __init__(self, malicious_ips: list[str], fp_prob: float):
         if not (0 < fp_prob < 1):
             raise ValueError("False positive probability must be between 0 and 1.")
@@ -22,33 +22,35 @@ class MaliciousIPFilter:
         self.num_items = num_items
         
         print(f"## IP Filter Initialized ##")
-        print(f"   - Initial Blacklist Size (n): {self.num_items}")
-        print(f"   - FP Probability (p): {fp_prob:.2%}")
-        print(f"   - Optimal Size (m): {self.size} bits")
-        print(f"   - Optimal Hashes (k): {self.hash_count}\n")
+        print(f"- Initial Blacklist Size (n): {self.num_items}")
+        print(f"- FP Probability (p): {fp_prob:.2%}")
+        print(f"- Optimal Size (m): {self.size} bits")
+        print(f"- Optimal Hashes (k): {self.hash_count}\n")
         
-        # Step 2: Populate the filter 
+        #Filling the bloom filter with malicious URLs and IP address. 
         self.populate_filter(malicious_ips)
 
     def _calculate_optimal_size(self, n, p):
-        """Calculates the optimal array size (m)."""
+        #Calculates the optimal array size (m).
         m = - (n * math.log(p)) / (math.log(2) ** 2)
         return int(math.ceil(m))
 
     def _calculate_optimal_hash_count(self, m, n):
-        """Calculates the optimal number of hash functions (k)."""
+        #Calculates the optimal number of hash functions (k).
         k = (m / n) * math.log(2)
         return int(math.ceil(k))
-
+    #DJB2 is a string hashing algorithm.
+    # To generate multiple independent hash values using only two base hashes.
+    # This improves performance and reduces code complexity.
     def _hash_djb2(self, s: str):
-        """Second hash function for double hashing (DJB2)."""
+        #Second hash function for double hashing (DJB2).
         hash_val = 5381
         for char in s:
             hash_val = ((hash_val << 5) + hash_val) + ord(char)
         return hash_val
 
     def _get_hashes(self, item: str):
-        """Generates k indices using Double Hashing: (h1 + i * h2) mod m."""
+        #Generates k indices using Double Hashing: (h1 + i * h2) mod m.
         # item is treated as a string (the IP address)
         hash1 = hash(item)
         hash2 = self._hash_djb2(item)
@@ -57,19 +59,19 @@ class MaliciousIPFilter:
             yield (hash1 + i * hash2) % self.size
             
     def add(self, item: str):
-        """Adds a single IP address string to the filter."""
+        #Adds a single IP address string to the filter.
         for digest in self._get_hashes(item):
             self.bit_array[digest] = 1
 
     def populate_filter(self, ips: list[str]):
-        """Populates the filter with the initial list of IP addresses."""
+        #Populates the filter with the initial list of IP addresses.
         print("   - Populating filter...")
         for ip in ips:
             self.add(ip)
         print("   - Filter successfully populated with initial blacklist.")
 
     def __contains__(self, item: str):
-        """Checks if an IP address is possibly malicious (enables 'ip in filter' syntax)."""
+        #Checks if an IP address is possibly malicious (enables 'ip in filter' syntax).
         for digest in self._get_hashes(item):
             if self.bit_array[digest] == 0:
                 return False
